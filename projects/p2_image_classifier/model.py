415f85cd09e996c2c2045876ea9d4f535140854f
Eric Seidler <eric.seidler@gmail.com>
2020-02-18 13:38:11 -0600

    remove commented code

--- projects/p2_image_classifier/model.py
+++ projects/p2_image_classifier/model.py
@@ -5,21 +5,6 @@ import torch
 import torch.nn.functional as F
 import warnings
 
-
-# class TwoLayerClassifier(nn.Module):
-#     def __init__(self, inputs, hidden_one, hidden_two, outputs, p):
-#         super().__init__()
-#         self.fc1 = nn.Linear(inputs, hidden_one)
-#         self.fc2 = nn.Linear(hidden_one, hidden_two)
-#         self.fc3 = nn.Linear(hidden_two, outputs)
-#         self.dropout = nn.Dropout(p=p)
-
-#     def forward(self, x):
-#         x = x.view(x.shape[0], -1)
-#         x = self.dropout(F.relu(self.fc1(x)))
-#         x = self.dropout(F.relu(self.fc2(x)))
-#         x = F.log_softmax(self.fc3(x), dim=1)
-#         return x
     
 class Classifier(nn.Module):
     def __init__(self, inputs, hidden_inputs, outputs, p):
@@ -46,14 +31,11 @@ ALLOWED_ARCHS = [
     'densenet169',
     'densenet201'
 ]
-    
-    
-# def build_two_layer_classifer(inputs=2208, hidden_one=512, hidden_two=256, outputs=102, p=0.2):
-#     return TwoLayerClassifier(inputs, hidden_one, hidden_two, outputs, p)
 
 
 def is_resnet(model_name):
     return model_name.startswith('resnet')
+
 
 def build_classifier(inputs=2208, hidden=512, outputs=102, p=0.2):
     return Classifier(inputs, hidden, outputs, p)
@@ -133,26 +115,6 @@ def save_checkpoint(model, epochs, optimizer, model_name, learning_rate, save_di
     checkpoint_loc = save_dir + '/checkpoint.pth'
     torch.save(checkpoint, checkpoint_loc)
     print(f'Checkpoint saved at {checkpoint_loc}')
-    
-        
-# def build_model_from_state_checkpoint(filepath):
-#     checkpoint = torch.load(filepath, map_location='cpu')
-#     # filter deprecated warnings (from pytorch download): https://stackoverflow.com/a/14463362/1113872
-#     with warnings.catch_warnings():
-#         warnings.simplefilter("ignore")
-#         pretrained = getattr(models, checkpoint['transfer_model_name'])(pretrained=True)
-#     classifier = build_two_layer_classifer(
-#         checkpoint['inputs'],
-#         checkpoint['hidden_in'],
-#         checkpoint['hidden_out'],
-#         checkpoint['outputs']
-#     )
-#     classifier.load_state_dict(checkpoint['classifier_state_dict'])
-#     setattr(pretrained, 'classifier', classifier)
-#     pretrained.class_to_idx = checkpoint['class_to_idx']
-#     pretrained.eval()
-    
-#     return pretrained, checkpoint
 
 
 def build_model_from_checkpoint(filepath):
